5f1e1367af23b307ecde9a2b22909c7676f9d26c
Jinane <jinanealayli@gmail.com>
2025-05-09 12:25:56 +0300

    deploy test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
@@ -5,6 +7,12 @@ import "./globals.css"
 import { Providers } from "./providers"
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
+
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
+import { loginSuccess, logout } from "@/lib/redux/slices/authSlice"
+import api from "@/lib/api"
+
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
@@ -17,11 +25,19 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    api.get("/auth/me")
+      .then((res) => dispatch(loginSuccess(res.data)))
+      .catch(() => dispatch(logout()))
+  }, [dispatch])
+
   return (
     <html lang="en">
       <body className={inter.className}>
         <Providers>{children}</Providers>
-         <ToastContainer position="top-right" autoClose={3000} />
+        <ToastContainer position="top-right" autoClose={3000} />
       </body>
     </html>
   )
